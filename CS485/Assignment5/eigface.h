3c346de69fd0c518bd4201bcb498d5eb964a5aca
josh <gleaso22@gmail.com>
2011-04-16 21:39:39 -0700

    first files

---
+++ CS485/Assignment5/eigface.h
@@ -0,0 +1,15 @@
+#ifndef JOSH_EIGFACE
+#define JOSH_EIGFACE
+
+#include <cv.h>
+#include <cvaux.h>
+#include <highgui.h>
+
+using namespace cv;
+
+class EiganFace
+{
+  // temp  
+};
+
+#endif // JOSH_EIGFACE
---
+++ CS485/Assignment5/train.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <cv.h>
+#include <cvaux.h>
+#include <highgui.h>
+#include "eigface.h"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
+
