08f028d3c56302e639c5cf858cd5cd023a62a0b8
josh <gleaso22@gmail.com>
2011-04-24 16:13:32 -0700

    used better thresholds for low rez ROC curve

--- CS485/Assignment5/tempB.py
+++ CS485/Assignment5/tempB.py
@@ -9,6 +9,9 @@ def drange(start, stop, step):
 		r += step
 
 os.system("rm out.txt");
-for x in drange(0.01,0.53,0.01):
+for x in drange(0.001,0.04,0.001):
+	print x
+	os.system("./testb traindata/train_L_b images/fb_L_images.txt " + str(x) + " >> out.txt")
+for x in drange(0.02,0.11,0.01):
 	print x
 	os.system("./testb traindata/train_L_b images/fb_L_images.txt " + str(x) + " >> out.txt")
